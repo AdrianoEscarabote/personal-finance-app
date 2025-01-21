7172a2848f32055a1b8c9261ef3fd3dc45c2edd3
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-01-21 11:32:26 -0300

    feat(storybook): add storybook-store to load states

---
+++ .storybook/storybook-store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit"
+import rootReducer from "../redux/rootReducer"
+import getMockState from "../utils/getMockState"
+
+const initialState = getMockState()
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: initialState,
+})
+
+export default store

527c3d23a97b79b4788ea39c18a9656b9505981a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-01-21 11:34:44 -0300

    feat(redux): add redux store & root reducer

---
+++ redux/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit"
+import financeSlice from "./finance/reducer"
+
+const rootReducer = combineReducers({
+  financeSlice,
+})
+
+export default rootReducer

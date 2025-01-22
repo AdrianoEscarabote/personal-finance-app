aeb8f3d437d26c00e2b631c48942d5b96b30cf6b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-01-22 16:47:59 -0300

    chore: installing tailwind scrollbar and configuring it as a plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import tailwindScrollbar from "tailwind-scrollbar"
 
 export default {
   content: [
@@ -34,5 +35,5 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [tailwindScrollbar],
+} satisfies Config

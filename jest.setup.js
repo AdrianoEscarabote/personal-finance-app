a91f4ec4ad0b7dd53053c9762dc311db172cc03b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-01-30 12:56:55 -0300

    chore(jest): configure tabbable mock in jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,19 @@
 import "@testing-library/jest-dom"
 
+const lib = jest.requireActual("tabbable")
+
+jest.mock("tabbable", () => ({
+  ...lib,
+  tabbable: (node, options) =>
+    lib.tabbable(node, { ...options, displayCheck: "none" }),
+  focusable: (node, options) =>
+    lib.focusable(node, { ...options, displayCheck: "none" }),
+  isFocusable: (node, options) =>
+    lib.isFocusable(node, { ...options, displayCheck: "none" }),
+  isTabbable: (node, options) =>
+    lib.isTabbable(node, { ...options, displayCheck: "none" }),
+}))
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({

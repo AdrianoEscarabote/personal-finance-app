b445230cb393000d89c65f34cf16eddab277b956
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-02-05 12:09:38 -0300

    chore(components): configuring variant withPrefix and withIcon

--- app/_components/input/index.tsx
+++ app/_components/input/index.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, Ref } from "react"
 import { InputProps } from "./InputProps"
-import Image from "next/image"
+import style from "./style.module.css"
 
 const Input = (
   {
@@ -24,24 +24,21 @@ const Input = (
       <input
         ref={ref}
         id={id}
-        className={`${variant === "withPrefix" ? "pl-8" : "pl-3"} text-preset-4 h-[45px] w-full rounded-lg border border-grey-500 text-grey-900`}
+        className={`${variant === "withPrefix" ? "pl-8" : "pl-3"} text-preset-4 h-[45px] w-full rounded-lg border border-grey-500 text-grey-900 ${variant === "withIcon" && style.icon_search}`}
         {...props}
       />
 
-      {showCaracterLeft && (
-        <span className="text-preset-5 mt-0.5 text-end text-grey-500">
-          30 characters left
-        </span>
+      {showCaracterLeft && props.maxLength && (
+        <p className="mt-1 text-right text-sm text-gray-500">
+          {props.maxLength - ((props.value as string)?.length || 0)} of{" "}
+          {props.maxLength} characters left
+        </p>
       )}
 
       {variant === "withPrefix" && (
         <div className="absolute left-5 top-[2rem]">
           <span className="text-preset-4 -z-40 text-beige-500">$</span>
         </div>
-      )}
-
-      {variant == "withIcon" && (
-        <Image src={"/images/icon-search.svg"} alt="" width={20} height={20} />
       )}
 
       {errors && errorMessage && (

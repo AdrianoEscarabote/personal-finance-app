d21e6448e51d763c83b0355c59ec76bbde498980
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-02-12 11:11:37 -0300

    chore(transactions): add transaction table tests

--- app/transactions/_components/transactionsTable/transactionsTable.test.tsx
+++ app/transactions/_components/transactionsTable/transactionsTable.test.tsx
@@ -1,8 +1,8 @@
 import getMockState from "@/utils/getMockState"
-import { render } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { legacy_configureStore as configureStore } from "redux-mock-store"
 import TransactionsTable from "."
+import { fireEvent, render, screen } from "@testing-library/react"
 
 const mockStore = configureStore([])
 
@@ -21,11 +21,36 @@ describe("transactionsTable", () => {
   afterAll(() => {
     jest.clearAllMocks()
   })
+
   it("should render correctly", () => {
     render(
       <Provider store={store}>
         <TransactionsTable />
       </Provider>,
     )
+  })
+
+  it("renders transaction categories", () => {
+    render(
+      <Provider store={store}>
+        <TransactionsTable />
+      </Provider>,
+    )
+
+    expect(screen.getAllByText("Groceries")).toBeTruthy()
+    expect(screen.getByText("Transportation")).toBeTruthy()
+  })
+
+  it("filters transactions based on search input", () => {
+    render(
+      <Provider store={store}>
+        <TransactionsTable />
+      </Provider>,
+    )
+    const searchInput = screen.getByPlaceholderText("Search transaction")
+    fireEvent.change(searchInput, { target: { value: "Aqua Flow Utilities" } })
+
+    expect(screen.getByText("Aqua Flow Utilities")).toBeTruthy()
+    expect(screen.queryByText("John Doe")).toBeFalsy()
   })
 })

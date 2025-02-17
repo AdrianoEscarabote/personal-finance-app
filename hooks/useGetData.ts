86db992ed0e91e32683a6b73ab2059b860992c5b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-02-17 17:04:53 -0300

    feat(hooks): add useGetData hook

---
+++ hooks/useGetData.ts
@@ -0,0 +1,32 @@
+import { setData } from "@/redux/finance/reducer"
+import { RootState } from "@/redux/reduxTypes"
+import axios from "axios"
+import { useEffect, useTransition } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+const useGetData = () => {
+  const dispatch = useDispatch()
+  const {} = useSelector((rootState: RootState) => rootState.financeSlice)
+  const [isPending, startTransition] = useTransition()
+
+  const handleGetData = () => {
+    startTransition(async () => {
+      const response = await axios.get("/data.json").then((res) => res.data)
+      if (response) {
+        startTransition(() => {
+          dispatch(setData(response))
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleGetData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return {
+    isPending,
+  }
+}
+
+export default useGetData

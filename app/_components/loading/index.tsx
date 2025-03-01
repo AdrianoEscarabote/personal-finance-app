725f35fcf1b212d152d6bc60212bc2d432600c5e
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-03-01 17:55:02 -0300

    chore(components): add theme prop to loading component

--- app/_components/loading/index.tsx
+++ app/_components/loading/index.tsx
@@ -1,6 +1,7 @@
+import { LoadingProps } from "./loadingProps"
 import style from "./style.module.css"
 
-const Loading = () => {
+const Loading = ({ theme }: LoadingProps) => {
   return (
     <svg className={style.spinner} viewBox="0 0 50 50">
       <circle
@@ -10,6 +11,7 @@ const Loading = () => {
         r="20"
         fill="none"
         strokeWidth="7"
+        stroke={`${theme === "dark" ? "#201f24" : "#fff"}`}
       ></circle>
     </svg>
   )

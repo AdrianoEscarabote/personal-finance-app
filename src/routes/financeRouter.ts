9787a68f30352daa3645bf8c5a48b5e1d143d872
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-03-20 10:58:35 -0300

    chore(routes): setup transaction route on finance router

--- src/routes/financeRouter.ts
+++ src/routes/financeRouter.ts
@@ -2,10 +2,12 @@ import express from "express"
 
 import budgetsRoute from "./finance/budgets"
 import potsRoute from "./finance/pots"
+import TransactionRoute from "./finance/transactions"
 
 const financeRouter = express.Router()
 
 financeRouter.use("/budgets", budgetsRoute)
 financeRouter.use("/pots", potsRoute)
+financeRouter.use("/transactions", TransactionRoute)
 
 export default financeRouter

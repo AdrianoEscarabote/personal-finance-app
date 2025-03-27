3d9fb5d510096b7d058e70bb6053e8842d545400
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-03-27 13:22:27 -0300

    feat(routes): add get data route

--- src/routes/financeRouter.ts
+++ src/routes/financeRouter.ts
@@ -1,6 +1,7 @@
 import express from "express"
 
 import budgetsRoute from "./finance/budgets"
+import getDataRoute from "./finance/get-data"
 import potsRoute from "./finance/pots"
 import TransactionRoute from "./finance/transactions"
 
@@ -9,5 +10,6 @@ const financeRouter = express.Router()
 financeRouter.use("/budgets", budgetsRoute)
 financeRouter.use("/pots", potsRoute)
 financeRouter.use("/transactions", TransactionRoute)
+financeRouter.use("/get-data", getDataRoute)
 
 export default financeRouter

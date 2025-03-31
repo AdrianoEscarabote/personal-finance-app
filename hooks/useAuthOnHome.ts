83a14f5c93b6b5de62d2ebe0b793d83920f7fbc0
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-03-31 15:28:41 -0300

    feat(hooks): add useAuthOnEntry & useAuthOnHome hook

---
+++ hooks/useAuthOnHome.ts
@@ -0,0 +1,36 @@
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+// Hook for home page
+const useAuthOnHome = () => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/user-authenticated`,
+          {
+            method: "GET",
+            credentials: "include",
+          },
+        )
+
+        if (response.status !== 200) {
+          router.push("/login")
+        }
+      } catch (error) {
+        console.error("Auth error on home:", error)
+        router.push("/login")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    checkAuth()
+  }, [router])
+
+  return { isLoading }
+}
+
+export default useAuthOnHome

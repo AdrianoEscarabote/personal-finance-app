448a4d3eb0bbd4001091524eeef0f62dc9454419
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-08-18 17:26:27 -0300

    feat: add theme provider to layout and dark mode support with new colors

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,6 +6,7 @@ import { Public_Sans } from "next/font/google"
 import Head from "next/head"
 import { Provider } from "react-redux"
 
+import { ThemeProvider } from "@/components/theme-provider"
 import store from "@/redux/store"
 
 import Sidebar from "./_components/sidebar"
@@ -21,18 +22,25 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Head>
         <title>Personal finance app - Overview</title>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <body className={`${PublicSans.className} antialiased`}>
-        <div className="flex w-full bg-beige-100">
-          <Sidebar />
-          <Provider store={store}>
-            <AppProvider>{children}</AppProvider>
-          </Provider>
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="dark:bg-grey-975 flex w-full bg-beige-100">
+            <Sidebar />
+            <Provider store={store}>
+              <AppProvider>{children}</AppProvider>
+            </Provider>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )

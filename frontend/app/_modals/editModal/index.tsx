b57b052d1fc5f20128973f45b37e3a651969c45a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-08-18 11:39:49 -0300

    refactor(modals): update withdraw & edit modal to use shadcn dialog and improve structure

--- frontend/app/_modals/editModal/index.tsx
+++ frontend/app/_modals/editModal/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { useDispatch } from "react-redux"
@@ -7,8 +6,8 @@ import Button from "@/app/_components/button"
 import ColorTag from "@/app/_components/colorTag"
 import Input from "@/app/_components/input"
 import SelectCategory from "@/app/_components/selectCategory"
+import { DialogDescription, DialogTitle } from "@/components/ui/dialog"
 import useDemoFetch from "@/hooks/useDemoFetch"
-import useEscClose from "@/hooks/useEscClose"
 import { editBudget, editPot } from "@/redux/finance/reducer"
 
 import { EditModalProps } from "./editModalProps"
@@ -22,7 +21,7 @@ const EditModal = ({
   data_edit_budget,
 }: EditModalProps) => {
   const dispatch = useDispatch()
-  useEscClose(closeModal)
+
   const [loading, setLoading] = useState(false)
   const { demoFetch } = useDemoFetch()
   const [theme, setTheme] = useState<string>("")
@@ -106,88 +105,72 @@ const EditModal = ({
   }, [])
 
   return (
-    <div
-      onClick={closeModal}
-      className={`fixed left-0 top-0 z-40 flex h-full min-h-screen w-full items-center justify-center overflow-y-scroll bg-black bg-opacity-50 p-5`}
-    >
-      <article
-        onClick={(e) => e.stopPropagation()}
-        className={`w-full max-w-[35rem] rounded-xl bg-white p-5 shadow-md transition-transform duration-300 lg:p-8`}
-      >
-        <div className="mb-5 flex items-center justify-between">
+    <article className={`w-full max-w-[35rem] rounded-xl bg-white`}>
+      <div className="mb-4 flex items-center justify-between">
+        <DialogTitle asChild>
           <h3 className="text-preset-1 text-grey-900">
             Edit {content === "pot" ? "Pot" : "Budget"}
           </h3>
-          <button
-            className="rounded-full"
-            onClick={closeModal}
-            data-testid="close_modal_button"
-          >
-            <Image
-              src={"/images/icon-close-modal.svg"}
-              alt=""
-              width={30}
-              height={30}
-            />
-          </button>
-        </div>
-        <p className="text-preset-4 mb-5 text-grey-500">
+        </DialogTitle>
+      </div>
+      <DialogDescription asChild>
+        <p className="text-preset-4 mb-4 text-grey-500">
           {content === "pot" &&
             "If your saving targets change, feel free to update your pots."}
           {content === "budget" &&
             "As your budgets change, feel free to update your spending limits."}
         </p>
-        <form onSubmit={onSubmit}>
-          <fieldset>
-            <legend className="sr-only">
-              Enter your new pot details below to update your pot.
-            </legend>
-            <div className="flex w-full flex-col gap-4">
-              {showPotName && (
-                <Input
-                  variant="basic"
-                  id="name"
-                  label="Pot Name"
-                  showCaracterLeft
-                  errors={errors.name?.message ? true : false}
-                  errorMessage={errors.name?.message as string}
-                  {...register("name", {
-                    required: "This field is required.",
-                  })}
-                />
-              )}
-              {showbudgetCategory && (
-                <SelectCategory
-                  setCategory={setSelectedCategory}
-                  category={data_edit_budget?.budget_category}
-                  label="Budget Category"
-                  dontFilter
-                />
-              )}
+      </DialogDescription>
+      <form onSubmit={onSubmit}>
+        <fieldset>
+          <legend className="sr-only">
+            Enter your new pot details below to update your pot.
+          </legend>
+          <div className="flex w-full flex-col gap-4">
+            {showPotName && (
               <Input
-                label="Maximum Spend"
-                variant="withPrefix"
-                id="maximum"
-                errors={errors.maximum?.message ? true : false}
-                errorMessage={errors.maximum?.message as string}
-                {...register("maximum", {
+                variant="basic"
+                id="name"
+                label="Pot Name"
+                showCaracterLeft
+                errors={errors.name?.message ? true : false}
+                errorMessage={errors.name?.message as string}
+                {...register("name", {
                   required: "This field is required.",
                 })}
               />
-              <ColorTag setTheme={setTheme} label={"Theme"} dontFilter />
-            </div>
-            <Button
-              variant="primary"
-              type="submit"
-              loading={loading}
-              disabled={loading}
-              label="Save Changes"
-              style={{ marginTop: "1.25rem" }}
+            )}
+            {showbudgetCategory && (
+              <SelectCategory
+                setCategory={setSelectedCategory}
+                category={data_edit_budget?.budget_category}
+                label="Budget Category"
+                dontFilter
+              />
+            )}
+            <Input
+              label="Maximum Spend"
+              variant="withPrefix"
+              id="maximum"
+              errors={errors.maximum?.message ? true : false}
+              errorMessage={errors.maximum?.message as string}
+              {...register("maximum", {
+                required: "This field is required.",
+              })}
             />
-          </fieldset>
-        </form>
-      </article>
-    </div>
+            <ColorTag setTheme={setTheme} label={"Theme"} dontFilter />
+          </div>
+          <Button
+            variant="primary"
+            type="submit"
+            loading={loading}
+            disabled={loading}
+            label="Save Changes"
+            style={{ marginTop: "1.25rem" }}
+          />
+        </fieldset>
+      </form>
+    </article>
   )
 }
 

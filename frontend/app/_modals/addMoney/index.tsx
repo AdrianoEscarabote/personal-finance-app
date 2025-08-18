57f77e98a1d4c8bcc346c2f47418d5a0ac222ed0
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-08-18 11:41:06 -0300

    refactor(modals): update add money & delete modal to use shadcn dialog and improve structure

--- frontend/app/_modals/addMoney/index.tsx
+++ frontend/app/_modals/addMoney/index.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { useDispatch } from "react-redux"
 
 import Button from "@/app/_components/button"
 import Input from "@/app/_components/input"
+import { DialogDescription, DialogTitle } from "@/components/ui/dialog"
 import useDemoFetch from "@/hooks/useDemoFetch"
-import useEscClose from "@/hooks/useEscClose"
 import { addMoney } from "@/redux/finance/reducer"
 
 import { AddMoneyProps } from "./addMoneyProps"
@@ -22,7 +21,7 @@ const AddMoney = ({
   const dispatch = useDispatch()
   const [loading, setLoading] = useState(false)
   const { demoFetch } = useDemoFetch()
-  useEscClose(closeModal)
+
   const progress = (total / target) * 100
   const {
     register,
@@ -57,109 +56,97 @@ const AddMoney = ({
   })
 
   return (
-    <div
-      onClick={closeModal}
-      className={`fixed left-0 top-0 z-40 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-50 p-5`}
-    >
-      <article
-        className="max-w-[35rem] rounded-xl bg-white p-8 shadow-md"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="mb-5 flex items-center justify-between">
+    <article className="max-w-[35rem] rounded-xl bg-white">
+      <div className="mb-5 flex items-center justify-between">
+        <DialogTitle asChild>
           <h3 className="text-preset-1 text-grey-900">Add to ‘{name}’</h3>
-          <button className="rounded-full" onClick={closeModal}>
-            <Image
-              src={"/images/icon-close-modal.svg"}
-              alt=""
-              width={32}
-              height={32}
-            />
-          </button>
-        </div>
+        </DialogTitle>
+      </div>
+      <DialogDescription asChild>
         <p className="text-preset-4 mb-5 text-grey-500">
           Add money to your pot to keep it separate from your main balance. As
           soon as you add this money, it will be deducted from your current
           balance.
         </p>
+      </DialogDescription>
 
-        <div className="mt-5">
-          <p className="text-preset-4 flex w-full items-center justify-between text-grey-500">
-            New Amount
-            <span className="text-preset-1 text-grey-900">
-              ${total.toFixed(2)}
-            </span>
-          </p>
+      <div className="mt-5">
+        <p className="text-preset-4 flex w-full items-center justify-between text-grey-500">
+          New Amount
+          <span className="text-preset-1 text-grey-900">
+            ${total.toFixed(2)}
+          </span>
+        </p>
 
-          <div className="relative mb-5 mt-2 h-2 w-full bg-beige-100">
-            <div
-              className="absolute z-10 h-2 w-8 rounded-s-full border-r-2 border-grey-100 bg-black"
-              style={{ width: `${progress}%` }}
-            ></div>
-            <div
-              style={{
-                width: `${Math.min(newProgress, 100)}%`,
-                backgroundColor: theme,
-              }}
-              className="absolute z-0 h-full rounded-full transition-all duration-300"
-            ></div>
-          </div>
-
-          <div className="mb-5 flex w-full items-center justify-between">
-            <p className="text-preset-5-bold text-green">
-              {newProgress.toFixed(2)}%
-            </p>
-            <p className="text-preset-5 text-grey-500">Target of ${target}</p>
-          </div>
+        <div className="relative mb-5 mt-2 h-2 w-full bg-beige-100">
+          <div
+            className="absolute z-10 h-2 w-8 rounded-s-full border-r-2 border-grey-100 bg-black"
+            style={{ width: `${progress}%` }}
+          ></div>
+          <div
+            style={{
+              width: `${Math.min(newProgress, 100)}%`,
+              backgroundColor: theme,
+            }}
+            className="absolute z-0 h-full rounded-full transition-all duration-300"
+          ></div>
         </div>
 
-        <form onSubmit={onSubmit}>
-          <fieldset className="flex flex-col gap-5">
-            <Input
-              variant="basic"
-              errors={errors.amount?.message ? true : false}
-              errorMessage={errors.amount?.message as string}
-              id="amount"
-              label="Amount to Add"
-              data-testid="amount_input"
-              {...register("amount", {
-                required: "This field is required",
-                pattern: {
-                  value: /^[0-9]+(\.[0-9]{1,2})?$/,
-                  message: "Only numbers are allowed",
-                },
-                min: {
-                  value: 0.01,
-                  message: "Amount must be greater than zero",
-                },
-                max: {
-                  value: target - total,
-                  message: "Amount must be less than the target",
-                },
-              })}
-              inputMode="numeric"
-              max={target - total}
-              onInput={(e) => {
-                const input = e.target as HTMLInputElement
-                input.value = input.value.replace(/[^0-9]/g, "")
+        <div className="mb-5 flex w-full items-center justify-between">
+          <p className="text-preset-5-bold text-green">
+            {newProgress.toFixed(2)}%
+          </p>
+          <p className="text-preset-5 text-grey-500">Target of ${target}</p>
+        </div>
+      </div>
 
-                const maxAmount = target - total
-                if (Number(input.value) > maxAmount) {
-                  input.value = maxAmount.toString()
-                }
-              }}
-            />
+      <form onSubmit={onSubmit}>
+        <fieldset className="flex flex-col gap-5">
+          <Input
+            variant="basic"
+            errors={errors.amount?.message ? true : false}
+            errorMessage={errors.amount?.message as string}
+            id="amount"
+            label="Amount to Add"
+            data-testid="amount_input"
+            {...register("amount", {
+              required: "This field is required",
+              pattern: {
+                value: /^[0-9]+(\.[0-9]{1,2})?$/,
+                message: "Only numbers are allowed",
+              },
+              min: {
+                value: 0.01,
+                message: "Amount must be greater than zero",
+              },
+              max: {
+                value: target - total,
+                message: "Amount must be less than the target",
+              },
+            })}
+            inputMode="numeric"
+            max={target - total}
+            onInput={(e) => {
+              const input = e.target as HTMLInputElement
+              input.value = input.value.replace(/[^0-9]/g, "")
 
-            <Button
-              type="submit"
-              loading={loading}
-              disabled={loading}
-              variant="primary"
-              label="Confirm Addition"
-            />
-          </fieldset>
-        </form>
-      </article>
-    </div>
+              const maxAmount = target - total
+              if (Number(input.value) > maxAmount) {
+                input.value = maxAmount.toString()
+              }
+            }}
+          />
+
+          <Button
+            type="submit"
+            loading={loading}
+            disabled={loading}
+            variant="primary"
+            label="Confirm Addition"
+          />
+        </fieldset>
+      </form>
+    </article>
   )
 }
 
--- frontend/app/_modals/deleteModal/index.tsx
+++ frontend/app/_modals/deleteModal/index.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image"
 import { useState } from "react"
 
 import Button from "@/app/_components/button"
-import useEscClose from "@/hooks/useEscClose"
+import {
+  DialogClose,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog"
 
 import { DeleteModalProps } from "./deleteModalProps"
 
@@ -12,7 +15,6 @@ const DeleteModal = ({
   onCancel,
   onConfirm,
 }: DeleteModalProps) => {
-  useEscClose(onCancel)
   const [loading, setLoading] = useState(false)
 
   const handleDelete = () => {
@@ -21,39 +23,29 @@ const DeleteModal = ({
   }
 
   return (
-    <div
-      onClick={onCancel}
-      className="fixed left-0 top-0 z-40 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-50 p-4"
-    >
-      <article
-        onClick={(e) => e.stopPropagation()}
-        className="max-w-[35rem] rounded-xl bg-white p-8 shadow-md"
-      >
-        <div className="mb-5 flex items-center justify-between">
+    <article className="max-w-[35rem] rounded-xl bg-white">
+      <div className="mb-4 flex items-center justify-between">
+        <DialogTitle asChild>
           <h3 className="text-preset-1 text-grey-900">Delete ‘{title}’?</h3>
-          <button className="rounded-full" onClick={onCancel}>
-            <Image
-              src={"/images/icon-close-modal.svg"}
-              alt=""
-              width={32}
-              height={32}
-            />
-          </button>
-        </div>
-        <p className="text-preset-4 mb-5 text-grey-500">{description}</p>
+        </DialogTitle>
+      </div>
+      <DialogDescription asChild>
+        <p className="text-preset-4 mb-4 text-grey-500">{description}</p>
+      </DialogDescription>
 
-        <div className="mt-5 flex flex-col gap-5">
-          <Button
-            variant="destroy"
-            label="Yes, Confirm Deletion"
-            loading={loading}
-            disabled={loading}
-            onClick={handleDelete}
-          />
+      <div className="mt-5 flex flex-col gap-5">
+        <Button
+          variant="destroy"
+          label="Yes, Confirm Deletion"
+          loading={loading}
+          disabled={loading}
+          onClick={handleDelete}
+        />
+        <DialogClose asChild>
           <Button variant="tertiary" label="No, Go Back" onClick={onCancel} />
-        </div>
-      </article>
-    </div>
+        </DialogClose>
+      </div>
+    </article>
   )
 }
 
